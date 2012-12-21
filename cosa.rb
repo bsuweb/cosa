3e853e7a34e0b4d3a64a492caff1f1cdc5b5a572
Matt Buresh <mattburesh@gmail.com>
2012-12-21 15:53:50 -0600

    Updated to reflect new name.

--- cosa.rb
+++ cosa.rb
@@ -6,7 +6,7 @@ require 'yaml'
 require 'uri'
 require './lib/valid'
 
-class Crawler
+class Cosa
 	attr_accessor :db, :urls, :links, :queue, :SHELF, :domain, :start_time
 
 	def initialize()
@@ -40,27 +40,27 @@ end
 #accessed within the seed time. If it has not,add that url to the
 #queue with the pattern set to '' and force set to 0.
 #
-# crawler - Crawler object
+# cosa - Cosa object
 #
-def seed(crawler)
-  to_crawl = crawler.urls.where{accessed < Time.now - crawler.SHELF}
+def seed(cosa)
+  to_crawl = cosa.urls.where{accessed < Time.now - cosa.SHELF}
   to_crawl.each do |row|
-    insert_data(crawler, crawler.queue, [row[:url], '', 0])
+    insert_data(cosa, cosa.queue, [row[:url], '', 0])
   end
 
-  return crawl_queue(crawler) unless crawler.queue.empty?
+  return crawl_queue(cosa) unless cosa.queue.empty?
 end
 
 #Get the first row in the queue and check to see if either the force flag is
 #on, or if the url has not been added to the urls table, or if it has been
 #added to the urls table AND the url has not been accessed within the seed time
 #
-# crawler - Crawler object
+# cosa - Cosa object
 #
-def crawl_queue(crawler)
+def crawl_queue(cosa)
   # Create a Sequel object that corresponds to the first item in the queue
   # table. 'row' gets that value.
-  row = crawler.queue.first
+  row = cosa.queue.first
   puts row
 
   # If the force column in the sequel object is set to true crawl the page.
@@ -68,26 +68,26 @@ def crawl_queue(crawler)
   # Elsif the sequel object is in the urls table, but has not been accessed
   # within the shelf time crawl the page.
   if row[:force] == 1
-    crawl_url(row[:id], crawler)
-  elsif !crawler.urls[:url => row[:url]]
-    crawl_url(row[:id], crawler)
-  elsif crawler.urls[:url => row[:url]] && Time.now - Time.parse(crawler.urls.where(:url => row[:url]).get(:accessed)) > crawler.SHELF
-    crawl_url(row[:id], crawler)
+    crawl_url(row[:id], cosa)
+  elsif !cosa.urls[:url => row[:url]]
+    crawl_url(row[:id], cosa)
+  elsif cosa.urls[:url => row[:url]] && Time.now - Time.parse(cosa.urls.where(:url => row[:url]).get(:accessed)) > cosa.SHELF
+    crawl_url(row[:id], cosa)
   end
   # Delete the sequel object from the queue table.
-  crawler.queue.where(:id => row[:id]).delete
+  cosa.queue.where(:id => row[:id]).delete
 end
 
-def crawl_url(queue_id, crawler)
+def crawl_url(queue_id, cosa)
 
-  item = crawler.queue[:id => queue_id]
+  item = cosa.queue[:id => queue_id]
   url = item[:url]
   last_accessed = Time.now
   parsed_links = []
   old_links = []
   type_array = []
 
-  if url.include?('http') && !url.include?(crawler.domain)
+  if url.include?('http') && !url.include?(cosa.domain)
     #url is external, url = url
     internal = false
   elsif url[-1, 10] == "index.html"
@@ -97,7 +97,7 @@ def crawl_url(queue_id, crawler)
   response = Typhoeus::Request.get(url,
                                   :timeout => 30000,
                                   :headers => {
-                                    'User-Agent' => "Crawler/0.1 ()"
+                                    'User-Agent' => "Cosa/0.1 ()"
                                   })
   url = response.effective_url
 
@@ -124,7 +124,7 @@ def crawl_url(queue_id, crawler)
   end
 
   if valid[1] == "html"
-    if url.include?(crawler.domain)
+    if url.include?(cosa.domain)
 
       # Iterate through the a, link, img, video, audio, script, and object elements on the page.
       Nokogiri::HTML(body).css('a', 'link', 'img', 'video', 'audio', 'script', 'object').each do |item|
@@ -135,14 +135,14 @@ def crawl_url(queue_id, crawler)
         # type_array array.
         if item[:href]
           if !item[:href].include?('#') && !item[:href].include?('mailto:') && item[:href] != "http://" && !item[:href].include?('@')
-            insert_links(crawler, item, url, :href, parsed_links, type_array)
+            insert_links(cosa, item, url, :href, parsed_links, type_array)
           end
         # Else if the element contains an scr attribute, add it to the
         # parsed_links array. Also add that element and it's 'tag' to the
         # type_array array.
         elsif item[:src]
           if item[:src][0..4] != 'data:'
-            insert_links(crawler, item, url, :src, parsed_links, type_array)
+            insert_links(cosa, item, url, :src, parsed_links, type_array)
           end
         end
       end
@@ -154,17 +154,17 @@ def crawl_url(queue_id, crawler)
       # If the entry for the current url in the links table is empty, add each
       # item from the parsed_links array to the links table with the format
       # :from_url => current_url, :to_url => parsed_links_item, :type => type
-      if crawler.links.where(:from_url => url).empty?
+      if cosa.links.where(:from_url => url).empty?
 
         parsed_links.each do |link|
           type = determine_type(link, type_array)
-          insert_data(crawler, crawler.links, [url, link, type[1]])
+          insert_data(cosa, cosa.links, [url, link, type[1]])
         end
       else
 
         # Else the entry for the current url in the links table is not empty.
         # Add each of those links to the old_links array.
-        crawler.links.where(:from_url => url).each { |link| old_links << link[:to_url] }
+        cosa.links.where(:from_url => url).each { |link| old_links << link[:to_url] }
       end
 
       # Find the differences between old_links and parsed_links to determine if
@@ -173,7 +173,7 @@ def crawl_url(queue_id, crawler)
       deleted_links = old_links - parsed_links
 
       deleted_links.each do |link|
-        crawler.links.where(:to_url => link).delete
+        cosa.links.where(:to_url => link).delete
       end
 
       new_links = remove_leading(new_links)
@@ -182,19 +182,19 @@ def crawl_url(queue_id, crawler)
 
         # If this item in the new_links array is not in the links table, add it
         # to the links table.
-        unless crawler.links.where(:from_url => url, :to_url => link)
-          insert_data(crawler, crawler.links, [url, link, type[1]])
+        unless cosa.links.where(:from_url => url, :to_url => link)
+          insert_data(cosa, cosa.links, [url, link, type[1]])
         end
 
         # If the current url's pattern field is blank, add this item from
         # new_links to the queue with a blank pattern and a force value of 0.
         if item[:pattern] == ''
-          insert_data(crawler, crawler.queue, [link, '', 0]) if check_duplicates(crawler, link) == true
+          insert_data(cosa, cosa.queue, [link, '', 0]) if check_duplicates(cosa, link) == true
 
         # Elsif the pattern is not blank and 'link' matches the pattern, add
         # link to the queue with the same pattern and force value.
         elsif item[:pattern] != '' && link.include?(item[:pattern])
-          insert_data(crawler, crawler.queue, [link, item[:pattern], item[:force]]) if check_duplicates(crawler, link) == true
+          insert_data(cosa, cosa.queue, [link, item[:pattern], item[:force]]) if check_duplicates(cosa, link) == true
         end
       end
     end
@@ -207,7 +207,7 @@ def crawl_url(queue_id, crawler)
     body = ''
   end
 
-  rec = crawler.urls.where(:url => url)
+  rec = cosa.urls.where(:url => url)
 
   puts "queue_id: #{ queue_id }
   item: #{ item }
@@ -216,12 +216,12 @@ def crawl_url(queue_id, crawler)
   content type: #{ content_type }
   content length: #{ content_length }
   status: #{ status }
-  runtime: #{ Time.now - crawler.start_time}"
+  runtime: #{ Time.now - cosa.start_time}"
 
-  if crawler.urls[:url => url]
+  if cosa.urls[:url => url]
     rec.update(:accessed => last_accessed, :response => body)
   else
-    insert_data(crawler, crawler.urls, [url, last_accessed, content_type, content_length, status, body, valid[1], valid[0]])
+    insert_data(cosa, cosa.urls, [url, last_accessed, content_type, content_length, status, body, valid[1], valid[0]])
   end
 
 end
@@ -229,14 +229,14 @@ end
 #Edits links being added to parsed_links and type_array so that they are not
 #added as 'relative links'.
 #
-# crawler       - Crawler object
+# cosa          - Cosa object
 # item          - Curent page being added
 # url           - Current url
 # type          - href or src
 # parsed_links  - array of links on the current url
 # type_array    - array containing links on the current page and their 'types'
 #
-def insert_links(crawler, item, url, type, parsed_links, type_array)
+def insert_links(cosa, item, url, type, parsed_links, type_array)
   item[type] = URI.join( url, URI.escape(item[type].gsub(/\s+/, '').strip, "[]()| ") ).to_s
   parsed_links << item[type]
   type_array << [item[type], item.name]
@@ -248,15 +248,15 @@ end
 #hasn't been accessed in the last day return true. Else return false. If true
 #is returned, add the link to the queue.
 #
-# crawler - Crawler object
+# cosa    - Cosa object
 # link    - Link being checked
 #
-def check_duplicates(crawler, link)
-  unless crawler.queue[:url => link] || link == crawler.domain + '/'
-    dataset = crawler.urls[:url => link]
+def check_duplicates(cosa, link)
+  unless cosa.queue[:url => link] || link == cosa.domain + '/'
+    dataset = cosa.urls[:url => link]
     if dataset.nil?
       return true
-    elsif Time.parse(dataset[:accessed]) < (Time.now - crawler.SHELF)
+    elsif Time.parse(dataset[:accessed]) < (Time.now - cosa.SHELF)
       return true
     else
       return false
@@ -266,7 +266,7 @@ end
 
 #Determines the type of a link based on its 'tag'
 #
-# link - The link to be tested
+# link       - The link to be tested
 # type_array - Array with the links, and their tag
 #
 def determine_type(link, type_array)
@@ -303,11 +303,11 @@ end
 #table you wish to insert data to. The key and value arrays are combined into
 #a hash object and the data is inserted into the correct table.
 #
-# crawler - Crawler object
-# table   - Table to insert data
+# cosa     - Cosa object
+# table    - Table to insert data
 # values   - Values to be inserted into table
 #
-def insert_data(crawler, table, values)
+def insert_data(cosa, table, values)
   queue = [:url, :pattern, :force]
   links = [:from_url, :to_url, :type]
   urls = [:url, :accessed, :content_type, :content_length, :status, :response, :validation_type, :valid]
@@ -318,41 +318,41 @@ def insert_data(crawler, table, values)
   # array(queue, links, or urls) and sets it as the key in the data_hash
   # Hash object. That key is given the value of values[i]. i, and k are
   # incremented and the process repeats itself.
-  if table == crawler.queue
+  if table == cosa.queue
     queue.each_with_index { |k,i| data_hash[k] = values[i] }
-  elsif table == crawler.links
+  elsif table == cosa.links
     links.each_with_index { |k,i| data_hash[k] = values[i] }
-  elsif table == crawler.urls
+  elsif table == cosa.urls
     urls.each_with_index { |k,i| data_hash[k] = values[i] }
   end
 
-  crawler.db.transaction do
+  cosa.db.transaction do
     table.insert(data_hash)
   end
 end
 
-crawler = Crawler.new()
+cosa = Cosa.new()
 
 if ARGV.length < 1
   # No Arguments, check each url in the urls table and add old urls to the
   # queue to be checked again.
-  seed(crawler)
+  seed(cosa)
 elsif ARGV.length > 1
   # 2 Arguments, gets the url to add to the queue and the pattern to use when
   # checking pages.
   options = { "url" => ARGV.shift, "pattern" => ARGV.shift }
   options["pattern"] = URI.join( options["url"], options["pattern"] ).to_s
-  insert_data(crawler, crawler.queue, [ options["url"], options["pattern"], 1 ])
+  insert_data(cosa, cosa.queue, [ options["url"], options["pattern"], 1 ])
 else
   # 1 Argument, gets the url to add to the queue.
   options = { "url" => ARGV.shift, "pattern" => ARGV.shift }
-  insert_data(crawler, crawler.queue, [ options["url"], '', 1 ])
+  insert_data(cosa, cosa.queue, [ options["url"], '', 1 ])
 end
 
 # Check the next item in the queue as long as the queue is not empty
 while true
-  if !crawler.queue.empty?
-    crawl_queue(crawler)
+  if !cosa.queue.empty?
+    crawl_queue(cosa)
   else
     break
   end
