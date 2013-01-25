6a64e8a9176ea2395d849541231d7eb435bf413a
Matt Buresh <mattburesh@gmail.com>
2013-01-25 10:29:51 -0600

    Added basic output and CLI options. Moved CLI to the Database class.

--- cosa.rb
+++ cosa.rb
@@ -13,12 +13,70 @@ class String
 end
 
 class Database
-  attr_accessor :db, :urls, :links, :queue, :SHELF, :domain, :start_time
+  attr_accessor :opts, :db, :urls, :links, :queue, :SHELF, :domain, :start_time, :output
 
   def initialize
+    opts = Trollop::options do
+      version " Cosa 0.1"
+      banner <<-EOS
+      Cosa is a simple web crawler that generates a database for use by other
+      tools and reports.
+
+      Usage:
+        ruby cosa.rb http://www.example.com
+          - Cosa will start at this address, and crawl every page on the site.
+
+        ruby cosa.rb http://www.example.com/directory/ /directory/page/
+          - Cosa will start at 'http://www.example.com/directory/', and then
+            only add links to the queue if they contain the pattern
+            'http://www.example.com/directory/page'.
+
+        ruby cosa.rb
+          - If you have already run Cosa, this will check the first link in the
+            urls table and see if it has been crawled within the shelf time
+            (default 1 day). If it hasn't, that URL will be added to the
+            queue and the URL will be recrawled.
+
+        Because Cosa stores the queue in the database, you can quit the program
+        at any time and when you restart it, it will begin where it left off.
+      EOS
+
+      opt :init, "Command-line tool for creating and saving a config file"
+      opt :add, "Add a URL to the queue", :type => :strings
+      opt :config, "If not specified, Cosa will use the default config if it exists", :type => :string
+      opt :crawl, "Start the crawler. Optional switches for silent or verbose output.", :type => :string
+      opt :broken, "List all URLs that contain broken links and their broken links."
+      opt :abandoned, "List all pages that are no longers linked to."
+      opt :invalid_html, "List pages with invalid html."
+      opt :css, "List all CSS URLs that are linked to."
+      opt :age, "List all URLs that are older than the given date.", :type => :string
+      opt :queue, "List the current queue."
+      opt :clear_queue, "Empty the queue"
+      opt :response_time, "List the URLs that took longer than <seconds> to responsd.", :type => :integer
+      opt :unresponsive, "List URLs that were not responsive."
+      opt :to, "List URLs that link to the given URL.", :type => :string
+      opt :from, "List URLs that the given URL links to.", :type => :string
+      opt :silent, "Silence all output."
+      opt :snapshot, "Export the entire site from cosa as an HTML snapshot to the given path.", :type => :string
+      opt :verbose, "Verbose output."
+    end
+  set_opts(opts)
+  end
+
+  def set_opts(opts)
+    puts opts
     # Load configuration file
     # Used to load the base domain to be crawled, and the path to the database
-    config = YAML::load( File.open( 'config.yaml') )
+    if opts[:config]
+      config = YAML::load( File.open(opts[:config]) )
+    else
+      config = YAML::load( File.open('config.yaml') )
+    end
+    # No idea if this is more 'better' that if/else
+    # opts[:config] ? config = YAML::load( File.open(opts[:config])) : YAML::load( File.open(opts['config.yaml']))
+
+    @output = "silent" if opts[:silent]
+    @output = "verbose" if opts[:verbose]
 
     @db = Sequel.connect(config['db_path'])
     @urls = db[:urls]
@@ -26,6 +84,7 @@ class Database
     @queue = db[:queue]
     @domain = config['domain']
     @start_time = Time.now
+    @output = "default"
 
     if config['shelf_life']
       @SHELF = config['shelf_life']
@@ -205,15 +264,18 @@ class Database
 
     rec = urls.where(:url => url)
 
-    puts "queue_id: #{ queue_id }
-    item: #{ item }
-    url: #{ url }
-    last_accessed: #{ last_accessed }
-    content type: #{ content_type }
-    content length: #{ content_length }
-    status: #{ status }
-    response time: #{ response_time }
-    total runtime: #{ Time.now - start_time}"
+    if output == "default"
+      print "\rURL: #{ url } | Queue: #{ queue_id } | Avg Req: 1 | Total: #{ Time.now - start_time } DONE"
+      $stdout.flush
+    elsif output == "verbose"
+      puts "QueueID: #{ queue_id }
+      Current URL: #{ url }
+      Last Accessed: #{ last_accessed }
+      Content Type: #{ content_type }
+      Status Code: #{ status }
+      Page Reponse Time: #{ response_time }
+      Total Time: #{ Time.now - start_time } \n"
+    end
 
     if urls[:url => url]
       rec.update(:accessed => last_accessed, :response => body)
@@ -328,7 +390,7 @@ class Database
 end
 
 crawler = Database.new()
-commands(crawler, cli)
+puts crawler.SHELF
 
 if ARGV.length < 1
   # No Arguments, check each url in the urls table and add old urls to the
