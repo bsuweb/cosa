37c18dfd85d00b153176761345f71bc45b7d484b
Matt Buresh <mattburesh@gmail.com>
2013-02-12 15:41:50 -0600

    Updated help information.

--- cli.rb
+++ cli.rb
@@ -20,24 +20,17 @@ class Database
             only add links to the queue if they contain the pattern
             'http://www.example.com/directory/page'.
 
-        [DEPRECATED]
-        ruby cosa.rb [-options]
-          - If you have already run Cosa, this will check the first link in the
-            urls table and see if it has been crawled within the shelf time
-            (default 1 day). If it hasn't, that URL will be added to the
-            queue and the URL will be recrawled.
-
         Because Cosa stores the queue in the database, you can quit the program
         at any time and when you restart it, it will begin where it left off.
       EOS
 
-      opt :init, "Command-line tool for creating and saving a config file"
-      opt :add, "Add a URL to the queue", :type => :strings
-      opt :config, "If not specified, Cosa will use the default config if it exists", :type => :string
-      opt :crawl, "Start the crawler.", :type => :strings
-      opt :broken, "List all URLs that contain broken links and their broken links."
-      opt :abandoned, "List all pages that are no longers linked to."
-      opt :invalid_html, "List pages with invalid html."
+      opt :init, "Command-line tool for creating and saving a config file."
+      opt :add, "Add a URL (or multiple URLs, separated by spaces) to the queue.", :type => :strings
+      opt :config, "Run Cosa with a given config file. Otherwise, Cosa will use the default config if it exists", :type => :string
+      opt :crawl, "Start the crawler. Look above for examples of usage.", :type => :strings
+      opt :broken, "List all URLs that contain broken links, and their broken links."
+      #opt :abandoned, "List all pages that are no longers linked to."
+      #opt :invalid_html, "List pages with invalid html."
       opt :css, "List all CSS URLs that are linked to."
       opt :age, "List all URLs that are older than the given date.", :type => :string
       opt :queue, "List the current queue."
@@ -46,9 +39,9 @@ class Database
       opt :unresponsive, "List URLs that were not responsive."
       opt :to, "List URLs that link to the given URL.", :type => :string
       opt :from, "List URLs that the given URL links to.", :type => :string
-      opt :silent, "Silence all output.", :short => 's'
-      opt :snapshot, "Export the entire site from cosa as an HTML snapshot to the given path.", :type => :string, :short => "-o"
-      opt :verbose, "Verbose output.", :short => "-v"
+      opt :silent, "Silence all output.", :short => 'S'
+      #opt :snapshot, "Export the entire site from cosa as an HTML snapshot to the given path.", :type => :string, :short => "-o"
+      opt :verbose, "Verbose output.", :short => "-V"
     end
 
     # Make sure :age date is valid
