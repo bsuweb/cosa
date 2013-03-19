b71a9ff8484ff53be0fc986fa1d2c862e9d68b14
Matt Buresh <mattburesh@gmail.com>
2013-03-19 14:33:50 -0500

    Fixed snapshot trying to create paths from files.

--- snapshot.rb
+++ snapshot.rb
@@ -14,23 +14,28 @@ class Snapshot
     Dir.chdir(Dir.pwd)
 
     # Get a list of all sites that contain the domain from the config file
-    paths = opts[:urls].where(Sequel.like(:url, "%#{ opts[:domain][7..-1] }%" ))
-    # Remove the domain from each url, add add each new url and its response to a hash
+    paths = opts[:urls].where(Sequel.like(:url, "%#{ opts[:domain][7..-1] }%"))
+    # Remove the domain from each url, add add each new url and its response
+    # to a hash as long as its status code is >= 200 and less than 400
     paths_hash = {}
-    paths.each { |x| paths_hash[x[:url].gsub(opts[:domain], "")] = x[:response] }
+    paths.each do |x|
+      if x[:status].to_i < 400 && x[:status].to_i >= 200
+        paths_hash[x[:url].gsub(opts[:domain], "")] = x[:response]
+      end
+    end
 
     paths_hash.each do |path|
       # if path ends in "/"
-      $LOG.debug("#{path[0]}")
-      if path[0][-1, 1] == "/"
+      $LOG.debug("#{ path[0] }")
+      # if path[0][-1, 1] == "/"
+      if File.extname(path[0]).empty?
+      # unless path[1].nil? && File.extname(path[0]).empty?
         create_directory("#{ opts[:path] }#{ path[0] }")
         create_file("index.html", path[1], "#{ opts[:path] }#{path[0]}")
       else
         path_array = path[0].reverse.split('/', 2).collect(&:reverse).reverse
-        new_path = path_array[0]
-        file_name = path_array[1]
-        create_directory("#{ opts[:path] }#{ new_path }")
-        create_file(file_name, path[1], "#{ opts[:path] }#{new_path}")
+        create_directory("#{ opts[:path] }#{ path_array[0] }")
+        create_file(path_array[1], path[1], "#{ opts[:path] }#{ path_array[0] }")
       end
     end
   end
@@ -45,7 +50,8 @@ class Snapshot
   end
 
   def create_directory(path)
-    unless Dir.exists? "#{path}"
+    arr = path.reverse.split('/', 2).collect(&:reverse).reverse
+    unless File.exists? "#{ arr[1] }"
       FileUtils.mkdir_p "#{ path }"
     end
   end
